84fcc6a1503729a3823d600ce6cc149143fefc4e
Anshul Bharti <124706074+AnshulBharti-1822@users.noreply.github.com>
2025-03-06 16:50:10 +0530

    Create Duplicate.java

---
+++ Duplicate.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; 
+public class Duplicate {
+
+	public static void main(String[] args) {
+		int arr[]= {1,1,2,3,3,4,5,6,6};
+		int len = arr.length;
+		int temp[]= new int[arr.length];
+		int j =0;
+		
+		for (int i=0; i<len-1;i++) {
+			if (arr[i] != arr[i+1]) {
+				temp[j++]= arr[i];
+				
+			}
+		}
+		
+		temp[j++]=arr[len-1];
+		for (int k=0; k<j ; k++) {
+			System.out.print(temp[k]);
+		}
+	}
+
+}
