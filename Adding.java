6ae0380218f7ec32a81c068f326a89df9cac4338
Anshul Bharti <124706074+AnshulBharti-1822@users.noreply.github.com>
2025-03-06 16:30:21 +0530

    Create Adding.java

---
+++ Adding.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Adding {
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the Numbers:");
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter number 1");
+		int a = sc.nextInt();
+		System.out.println("Enter number 2");
+		int b = sc.nextInt();
+		int sum = a+b;
+        System.out.print("Addition of two numbers is : ");
+     	System.out.println(sum);	
+     	
+	}
+	
+}
